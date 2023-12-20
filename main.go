9fdfe04dcebc670bd63a97719bea053cb50ba414
duanchenxi <duanchenxicharles@gmail.com>
2023-12-20 14:53:12 +0800

    update

--- main.go
+++ main.go
@@ -2,21 +2,35 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"github.com/bitly/go-simplejson"
 	"time"
 
-	"github.com/bitly/go-simplejson"
 	"github.com/patrickmn/go-cache"
 )
 
+// ProductFeatCache 产品特性缓存
+var productFeatCache *cache.Cache
+
+func init() {
+	//todo_dual_camera 修改过期时间
+	productFeatCache = cache.New(24*time.Hour, 24*time.Hour)
+}
 func main() {
-	d := dog{}
-	GetChanNosAndConsistency(context.TODO(), d.name)
+	// 假设有一个 map[string]int 变量
+	m := map[string]int{
+		"1": 1,
+		"2": 2,
+	}
+	a := 1
+	result, ok := m[string(a)]
+	fmt.Println(result, ok)
 }
 
 type dog struct {
 	weight int
-	name string
+	name   string
 }
 
 func goCache() {
@@ -24,12 +38,13 @@ func goCache() {
 	productFeatCache.SetDefault("1", dog{})
 	doginter, ok := productFeatCache.Get("1")
 	fmt.Println(ok)
-	fmt.Println(doginter.(dog).weight)
+	fmt.Println(doginter.(dog).name + "1")
 }
 
-func GetChanNosAndConsistency(context context.Context, ProductFeature string) (chanNos string, consistency int, err error) {
+func GetChanNosAndConsistency(context context.Context, ProductFeature string) (result map[string]int, err error) {
+	result = make(map[string]int)
 	if ProductFeature == "" {
-		return "1", 0, err
+		return
 	}
 	// 将查到的数据转为json
 	js, err := simplejson.NewJson([]byte(ProductFeature))
@@ -37,15 +52,22 @@ func GetChanNosAndConsistency(context context.Context, ProductFeature string) (c
 		fmt.Println(err, "New")
 	}
 
-	chanNos, err = js.Get("chan_nos").String()
+	cloudInfoChanMap, err := js.Get("cloud_info_chan_map").Map()
 	if err != nil {
 		fmt.Println(err)
-		chanNos = "0"
 	}
-
-	consistency, err = js.Get("chan_consistency").Int()
-	if err != nil {
-		fmt.Println(err)
+	for k, v := range cloudInfoChanMap {
+		value, ok := v.(json.Number)
+		if ok {
+			intValue, err := value.Int64()
+			if err == nil {
+				result[k] = int(intValue)
+			} else {
+				return nil, err
+			}
+		} else {
+			return
+		}
 	}
 	return
 }
--- main_test.go
+++ main_test.go
@@ -2,37 +2,32 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"testing"
 )
 
-func TestGetChanNosAndConsistency1(t *testing.T) {
+func TestGetChanNosAndConsistency(t *testing.T) {
 	type args struct {
 		context        context.Context
 		ProductFeature string
 	}
 	tests := []struct {
-		name            string
-		args            args
-		wantChanNos     string
-		wantConsistency int
-		wantErr         bool
+		name       string
+		args       args
+		wantResult map[string]int
+		wantErr    bool
 	}{
 		// TODO: Add test cases.
-		{"name1", args{context: context.TODO(), ProductFeature: "{\"feature\": [4107,4112],\"chan_nos\":\"[1,2,3]\",\"chan_consistency\":1}"}, "[1,2,3]", 1, false},
-		// {"name2", args{context: context.TODO(), ProductFeature: ""}, "1", 0, false},
+		{"name1", args{context: context.TODO(), ProductFeature: "{\"feature\": [4107,4112],\"chan_nos\":\"[1,2,3]\",\"cloud_info_chan_map\": {\"1\": 1,\"2\": 2}}"}, nil, false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			gotChanNos, gotConsistency, err := GetChanNosAndConsistency(tt.args.context, tt.args.ProductFeature)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("GetChanNosAndConsistency() error = %v, wantErr %v", err, tt.wantErr)
-
+			gotResult, err := GetChanNosAndConsistency(tt.args.context, tt.args.ProductFeature)
+			if err != nil {
+				fmt.Printf("test err: %v\n", err)
 			}
-			if gotChanNos != tt.wantChanNos {
-				t.Errorf("GetChanNosAndConsistency() gotChanNos = %v, want %v", gotChanNos, tt.wantChanNos)
-			}
-			if gotConsistency != tt.wantConsistency {
-				t.Errorf("GetChanNosAndConsistency() gotConsistency = %v, want %v", gotConsistency, tt.wantConsistency)
+			for k, v := range gotResult {
+				fmt.Printf("key: %v,value: %v\n", k, v)
 			}
 		})
 	}
