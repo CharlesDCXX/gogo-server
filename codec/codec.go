abea7ba5b14e319e75a06ace80f629531a56b49e
duanchenxi <duanchenxicharles@gmail.com>
2023-12-28 11:03:54 +0800

    tijiao

---
+++ codec/codec.go
@@ -0,0 +1,21 @@
+package codec
+
+import "io"
+
+type Header struct {
+	ServiceMethod string
+	Seq           uint64
+	Error         string
+}
+
+type Codec interface {
+	io.Closer
+	ReadHeader(*Header) error
+	ReadBody(interface{}) error
+	Write(*Header, interface{})
+}
+
+type NewCodeFunc func (io.ReadWriteCloser) Codec
+
+type Type string
+
