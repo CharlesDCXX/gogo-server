b875ed560c2ec31156f4ae202a0d4d0d5a95aee0
Plt-段晨曦 <duanchenxi@haique-tech.com>
2023-12-28 11:02:44 +0800

    init

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 # gogo-server
-
-use nutsdb
--- main.go
+++ main.go
@@ -1,22 +1,9 @@
 package main
 
 import (
-	"log"
-	"net"
+	"fmt"
 )
 
-func startServer(addr chan string) {
-	l, err := net.Listen("tcp", ":0")
-	if err != nil {
-		log.Fatal("network error:", err)
-	}
-	log.Println("start rpc server on", l.Addr())
-	addr <- l.Addr().String()
-	
-
-}
 func main() {
-	log.SetFlags(0)
-	addr := make(chan string)
-	log.Println(addr)
+	fmt.Println("hello")
 }
