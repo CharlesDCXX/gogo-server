e785aeb3eab1e4f1cbb3968b80855cd8cc69a821
Plt-段晨曦 <duanchenxi@haique-tech.com>
2024-01-08 14:40:13 +0800

    day2 finish

--- main.go
+++ main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"net"
+	"sync"
 	"time"
 
-	"gogo-server.com/dcx/codec"
+	"gogo-server.com/dcx/client"
 	"gogo-server.com/dcx/server"
 )
 
@@ -27,35 +27,24 @@ func main() {
 	addr := make(chan string)
 	go startServer(addr)
 
-	// in fact, following code is like a simple geerpc client
-	conn, _ := net.Dial("tcp", <-addr)
-	defer func() { _ = conn.Close() }()
+	client, _ := client.Dial("tcp", <-addr)
+	defer func() { _ = client.Close() }()
 
 	time.Sleep(time.Second)
-	// send options
-	err := json.NewEncoder(conn).Encode(server.DefaultOption)
-
-	if err != nil {
-		log.Panicln("Encode DefaultOption:", err)
-	}
-	cc := codec.NewGobCodec(conn)
 	// send request & receive response
+	var wg sync.WaitGroup
+	// send options
 	for i := 0; i < 5; i++ {
-		h := &codec.Header{
-			ServiceMethod: "Foo.Sum",
-			Seq:           uint64(i),
-		}
-		err := cc.Write(h, fmt.Sprintf("geerpc req %d", h.Seq))
-		if err != nil {
-			log.Println("Write:", err)
-		}
-		_ = cc.ReadHeader(h)
-		var reply string
-		err = cc.ReadBody(&reply)
-		if err != nil {
-			log.Println("ReadBody:", err)
-		}
-		log.Println("reply:", reply)
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			args := fmt.Sprintf("geerpc req %d", i)
+			var reply string
+			if err := client.Call("Foo.Sum", args, &reply); err != nil {
+				log.Fatal("call Foo.Sum error:", err)
+			}
+			log.Println("reply:", reply)
+		}(i)
 	}
-	time.Sleep(time.Second * 10)
+	wg.Wait()
 }
